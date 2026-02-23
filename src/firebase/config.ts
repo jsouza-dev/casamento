b99d7033d988fd8fd31a3d50d7718439656388f4
Joao Souza <joaossouza2628@gmail.com>
2026-02-23 13:36:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7840210066-b99d2",
+  "appId": "1:874405916499:web:2febf2f9155264883f3ba6",
+  "apiKey": "AIzaSyByr_DM5dmUUPOYfiKx1qNnQFQa0swJFOI",
+  "authDomain": "studio-7840210066-b99d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "874405916499"
+};
